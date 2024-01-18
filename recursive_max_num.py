3e33c9c245b0f7b503bfdb7b5b0e7c90ed7ae3f8
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-01-17 23:11:45 -0800

    Chapter 4: recursive max num

---
+++ recursive_max_num.py
@@ -0,0 +1,7 @@
+def max(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    sub_max = max(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
+
+print(max([5, 2, 8, 4]))
